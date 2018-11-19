46caa59b8cc58e69d5b178df08a465dd55726856
Robby Renz <nxgrenz@gmail.com>
2018-11-19 12:40:13 -0600

    add starting html, js and pdf files for assignment 4

---
+++ assignments/assignment-4/Q1.js
@@ -0,0 +1,6 @@
+
+function outputCount(count) {
+    document.getElementById('totalSquares').innerHTML = `There are ${count} perfect squares`;
+}
+
+//JS CODE START
---
+++ assignments/assignment-4/Q2.js
@@ -0,0 +1,11 @@
+let url = "104.196.146.24/";
+
+function wsUrl() {
+    return `ws://${url}`
+}
+
+function httpUrl() {
+    return `http:/${url}`;
+}
+
+//JS CODE START
