0599c285ad4f07624cb37ea33a5b1658238d047b
Robby Renz <nxgrenz@gmail.com>
2018-11-25 18:28:33 -0600

    update IP address of Q2.js for assignment 4

--- assignments/assignment-4/Q2.js
+++ assignments/assignment-4/Q2.js
@@ -1,11 +1,11 @@
-let url = "104.196.146.24/";
+let url = "165.227.32.181/";
 
 function wsUrl() {
     return `ws://${url}`
 }
 
 function httpUrl() {
-    return `http:/${url}`;
+    return `http://${url}`;
 }
 
 //JS CODE START
